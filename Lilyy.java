027ec9c4788add3718e79c7dcd081034d2c6484a
Lilyan Hatim Hassan <lilyanhatim@gmail.com>
2024-09-04 08:42:04 +0300

    Add files via upload

---
+++ Lilyy.java
@@ -0,0 +1,12 @@
+
+
+public class Lilyy {
+    public static void main(String[] args) {
+System.out.println("    J    A    V             V    A");
+System.out.println("    J    AA    V          V      AA");
+System.out.println("J   J   A A A    V       V      A A A  ");
+System.out.println(" J  J  A      A    V   V       A     A  ") ;
+System.out.println("   J   A       A     V        A       A") ;       
+    }
+    
+}
