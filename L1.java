7b6ce84e9c23098c0ef5bd489d934dddf89ec85e
Lilyan Hatim Hassan <lilyanhatim@gmail.com>
2024-09-07 15:35:49 +0300

    Add files via upload

---
+++ L1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.l1;
+
+import java.util.Scanner;
+
+public class L1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What programming languages do you know?");
+        String proLang = input.nextLine();
+        System.out.println("That's great ! You can use " + proLang + " to develop our project!");
+    }
+}
